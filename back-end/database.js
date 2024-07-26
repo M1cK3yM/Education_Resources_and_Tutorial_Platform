17893f8860796b36406ccb3cec6a431c75f2b0cf
akimmk <damtewmikiyas@gmali.com>
2024-07-26 15:53:24 +0300

    database connected

---
+++ back-end/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://localhost:27017/eduDB";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
