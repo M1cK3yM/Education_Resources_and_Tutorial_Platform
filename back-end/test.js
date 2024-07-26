c4412ebc53bfa50108cf181ac8b0569294a92486
akimmk <damtewmikiyas@gmali.com>
2024-07-26 15:54:24 +0300

    test file added

---
+++ back-end/test.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const connectDB = require("./database");
+const eventRoutes = require("./src/routes/eventRoute");
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Use the event routes
+app.use("/", eventRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server  running  port ${PORT}`);
+});
