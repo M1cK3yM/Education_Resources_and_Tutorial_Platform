2ece97863e43089ddc8e42bb019c47dd244f1a3f
akimmk <damtewmikiyas@gmali.com>
2024-07-27 12:43:50 +0300

    event delete added

--- back-end/src/controllers/eventController.js
+++ back-end/src/controllers/eventController.js
@@ -63,12 +63,13 @@ const updateEvent = async (req, res) => {
 };
 
 const deleteEvent = async (req, res) => {
+  console.log("Deleting event by the id : ", req.params.id);
   try {
     const event = await Event.findByIdAndDelete(req.params.id);
     if (!event) {
       return res.status(404).json({ message: "Event not found" });
     }
-    res.json({ message: "Event deleted successfully" });
+    res.status(200).json({ message: "Event deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
