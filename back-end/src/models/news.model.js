40c44b1ea05b6fc1748cf646c2701e8a46ed5296
sensoiyl <asiratyitbarek@gmail.com>
2024-07-27 13:09:13 +0300

    news

---
+++ back-end/src/models/news.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const newsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+ author: {
+    type: String,
+    required: true,
+  },
+  publicationdate: {
+    type: Date,
+    required: true,
+  },
+ updateddate: {
+    type: Date,
+    required: true,
+  },
+ category: {
+    type: Date,
+    required: true,
+  },
+ tags: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
+
+const News = mongoose.model("News", newsSchema);
+
+module.exports = News;
