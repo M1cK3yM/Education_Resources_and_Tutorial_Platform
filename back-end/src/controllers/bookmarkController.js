eb2cc20899e1dedc9a4d14de7dc29cc80cbc479f
akimmk <damtewmikiyas@gmail.com>
2024-07-30 21:06:34 +0300

    bookmark controller

---
+++ back-end/src/controllers/bookmarkController.js
@@ -0,0 +1,44 @@
+const Bookmark = require("../models/bookmark.model");
+
+const addBookmark = async (req, res) => {
+  const bookmark = new Bookmark({
+    userId: req.user.id,
+    eventId: req.params.eventId,
+  });
+  try {
+    const newBookmark = bookmark.save();
+    res.status(201).json(newBookmark);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const getBookmarks = async (req, res) => {
+  try {
+    const bookmarks = await Bookmark.find({ userId: req.user.id });
+    res.status(200).json(bookmarks);
+  } catch (err) {
+    res.status(500).json({ message: err.meassage });
+  }
+};
+
+const deleteBookmark = async (req, res) => {
+  try {
+    const bookmark = await Bookmark.findOneAndDelete({
+      userId: req.user.id,
+      eventId: req.params.eventId,
+    });
+    if (!bookmark) {
+      return res.status(400).json({ message: "Bookmark not found" });
+    }
+    res.status(200).json({ message: "Bookmark deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.meassge });
+  }
+};
+
+module.exports = {
+  addBookmark,
+  deleteBookmark,
+  getBookmarks,
+};
