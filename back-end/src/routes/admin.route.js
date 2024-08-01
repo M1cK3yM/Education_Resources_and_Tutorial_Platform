ceb84ede00a82f62de675b14d890cd7dac3c34a0
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-01 03:10:57 +0300

    add admin login
    it is just one line in admin.route.js

--- back-end/src/routes/admin.route.js
+++ back-end/src/routes/admin.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/auth.controller");
 const adminController = require("../controllers/admin.controller");
 const { authorizeJwt, isAdmin } = require("../middleware/auth.middleware");
 
 router.get("/", authorizeJwt, isAdmin, adminController.getAllUsers);
 router.get("/:id", authorizeJwt, isAdmin, adminController.getUserById);
 router.get("/role/:role", authorizeJwt, isAdmin, adminController.getUserByRole);
+router.post("/login", authorizeJwt, isAdmin, authController.loginAccount);
 
 module.exports = router;
