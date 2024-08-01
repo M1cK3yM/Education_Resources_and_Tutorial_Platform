d1eb2e66d5577b11bf49e070a8559f0b8aef6461
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-01 14:42:41 +0300

    add index js for route and update on server js
    from now on all routers imported and exported in routes/index.js

---
+++ back-end/src/routes/index.js
@@ -0,0 +1,17 @@
+const adminRoutes = require("./admin.route");
+const authRoutes = require("./auth.route");
+const bookmarkRoutes = require("./bookmarkRoute");
+const eventRoutes = require("./eventRoute");
+const newsRoutes = require("./news.route");
+const resourceRoutes = require("./resourceRoute");
+const userRoutes = require("./user.route");
+
+module.exports = {
+  adminRoutes,
+  authRoutes,
+  bookmarkRoutes,
+  eventRoutes,
+  newsRoutes,
+  resourceRoutes,
+  userRoutes,
+};
