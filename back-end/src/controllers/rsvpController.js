8329fd28a77df133b972e7821ab8c179c533a8e8
akimmk <damtewmikiyas@gmail.com>
2024-08-03 22:25:59 +0300

    rsvp added

--- back-end/src/controllers/rsvpController.js
+++ back-end/src/controllers/rsvpController.js
@@ -1,27 +1,69 @@
-const RSVP = require('../models/rsvp.model');
+const Event = require("../models/event.model");
 
-const addRSVP = async (req, res) => {
-  const { userId, eventId, status, notes } = req.body;
-  const rsvp = new RSVP({ userId, eventId, status, notes });
+const addRsvp = async (req, res) => {
+  const { eventId } = req.body;
 
   try {
-    const newRSVP = await rsvp.save();
-    res.status(201).json(newRSVP);
+    const user = res.locals.user;
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    // Check if the user has already RSVPed
+    if (event.rsvp.includes(user._id)) {
+      return res.status(400).json({ message: "User already RSVPed" });
+    }
+
+    event.rsvp.push(user._id);
+    await event.save();
+
+    res
+      .status(201)
+      .json({ message: "RSVP added successfully", rsvp: event.rsvp });
   } catch (err) {
-    res.status(400).json({ message: err.message });
+    res.status(500).json({ message: "Server error" });
+    console.log(err.message);
   }
 };
 
-const getRSVPsByEvent = async (req, res) => {
+const deleteRsvp = async (req, res) => {
+  const { eventId } = req.body;
+
   try {
-    const rsvps = await RSVP.find({ eventId: req.params.eventId }).populate('userId', 'name');
-    res.status(200).json(rsvps);
+    const user = res.locals.user;
+    const event = await Event.findById(eventId);
+
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    if (event.rsvp.length === 0) {
+      return res.status(404).json({ message: "RSVP list is empty" });
+    }
+
+    // Find the index of the user in the RSVP list
+    const rsvpIndex = event.rsvp.indexOf(user._id);
+
+    if (rsvpIndex !== -1) {
+      event.rsvp.splice(rsvpIndex, 1);
+      await event.save();
+
+      return res.status(200).json({
+        message: "RSVP removed successfully",
+        rsvp: event.rsvp,
+      });
+    }
+
+    return res.status(404).json({ message: "User not RSVPed" });
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: "Server error" });
+    console.log(err.message);
   }
 };
 
 module.exports = {
-  addRSVP,
-  getRSVPsByEvent
+  addRsvp,
+  deleteRsvp,
 };
--- back-end/src/routes/rsvpRoute.js
+++ back-end/src/routes/rsvpRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const rsvpController = require("../controllers/rsvpController");
+const { authorizeJwt } = require("../middleware/auth.middleware");
 
-router.get("/", rsvpController.addRSVP);
-router.post("/:eventId", rsvpController.getRSVPsByEvent);
+router.post("/", authorizeJwt, rsvpController.addRsvp);
+router.delete("/", authorizeJwt, rsvpController.deleteRsvp);
 
 module.exports = router;
