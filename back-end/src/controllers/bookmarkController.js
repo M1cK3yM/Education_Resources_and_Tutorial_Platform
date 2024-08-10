0dd3d52fa11dfafb58ccc2a2b049ebb4a61ce1ab
akimmk <damtewmikiyas@gmail.com>
2024-08-10 09:31:45 +0300

    error message

--- back-end/src/controllers/bookmarkController.js
+++ back-end/src/controllers/bookmarkController.js
@@ -28,7 +28,8 @@ const getBookmarks = async (req, res) => {
     const user = res.locals.user;
     res.status(200).json(user.bookmark);
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ message: 'Server Error' });
+    console.error(err);
   }
 };
 
