fab554dc7e726cc3a202594827d0d80d46b3318b
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-14 02:50:04 +0300

    add search bar and typewriter-effect

--- front-end/UniHub Ethiopia/src/pages/homePage.jsx
+++ front-end/UniHub Ethiopia/src/pages/homePage.jsx
@@ -1,9 +1,37 @@
-function HomePage() {
-  return (
-    <div className="pt-16">
-      <h1>Home</h1>
-    </div>
-  );
-}
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+// import { Card } from "@/components/ui/card";
+import Typewriter from "typewriter-effect";
 
-export default HomePage;
+const Homepage = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center pt-20 w-full">
+      <section className="flex pt-16 w-full flex-col items-center">
+        <h1
+          className=" text-5xl lg:text-6xl sm:text-2xl md:text-4xl font-bold flex"
+          style={{ fontFamily: "Montserrat" }}
+        >
+          Search For{" "}
+          <span className="ps-5 text-purple-700">
+            <Typewriter
+              options={{
+                strings: ["PDF/ PPT", "Modules", "Tutorials", "Chapters"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </h1>
+
+        <div className="flex pt-5 mt-16 w-2/5 items-center h-36">
+          <Input className="h-10" type="text" placeholder="Search" />
+          <Button className="ms-2 h-12" type="submit">
+            Search
+          </Button>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Homepage;
