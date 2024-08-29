1b598ab22a3f68a42198db564719db89ea7fd327
akimmk <damtewmikiyas@gmail.com>
2024-08-29 15:08:48 +0300

    minor change on archived page

--- back-end/src/routes/archivedEventRoute.js
+++ back-end/src/routes/archivedEventRoute.js
@@ -18,7 +18,7 @@ router.get("/:id", async (req, res) => {
     if (!archivedEvent) {
       return res.status(404).json({ message: "Archived Event Not Found" });
     }
-    res.status(200).json({ message: "Archived Event" });
+    res.status(200).json(archivedEvent);
   } catch (err) {
     res.status(500).json({ message: "Server Error" });
     console.error(err);
